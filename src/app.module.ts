73e1082b406bda6b4b1718460aef2c102b7b3b8a
Vitor Gouvea <112331228+gouveavitorr@users.noreply.github.com>
2025-09-12 20:09:27 -0300

    feat: include employee and company into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { LoanModule } from './loan/loan.module';
+import { EmployeeModule } from './employee/employee.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, LoanModule],
+  imports: [UserModule, PrismaModule, LoanModule, EmployeeModule, CompanyModule],
 })
 export class AppModule {}
