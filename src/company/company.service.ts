f886763b8c98162aaeba220ffeaaba399322e2c8
Vitor Gouvea <112331228+gouveavitorr@users.noreply.github.com>
2025-09-12 20:08:03 -0300

    feat: add company module and service

---
+++ src/company/company.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import {Company} from "@prisma/client"
+
+@Injectable()
+export class CompanyService{
+    constructor(private prisma: PrismaService){}
+
+    async findById(id: Company['id']){
+        return await this.prisma.company.findUnique({
+            where: {
+                id,
+            }
+        })
+    }
+}
