8e4b90466f5824d40ecdc263cfa17d813d35afbd
Vitor Gouvea <112331228+gouveavitorr@users.noreply.github.com>
2025-09-12 20:08:19 -0300

    feat: add employee module

---
+++ src/employee/employee.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import {Employee} from "@prisma/client"
+
+@Injectable()
+export class EmployeeService{
+    constructor(private prisma: PrismaService){}
+
+    async findById(id: Employee['id']){
+        return await this.prisma.employee.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+}
