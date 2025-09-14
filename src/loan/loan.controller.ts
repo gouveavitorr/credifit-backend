6a74361d9572e991cce27f57150b7d10a2b6c202
Vitor Gouvea <112331228+gouveavitorr@users.noreply.github.com>
2025-09-14 00:23:16 -0300

    feat: add loan controller, finish create loan function

---
+++ src/loan/loan.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { LoanService } from './loan.service';
+import { CreateLoanDto } from './dto/CreateLoanDto';
+
+@Controller('loan')
+export class LoanController {
+  constructor(private readonly loanService: LoanService) {}
+
+  @Get(':employeeId')
+  async findByEmployeeId(@Param('employeeId') employeeId: string) {
+    const loan = await this.loanService.findByEmployeeId(employeeId);
+
+    if (!loan) {
+      throw new NotFoundException('Loan with given id was not found');
+    }
+
+    return loan;
+  }
+
+  @Get(':companyId')
+  async findByCompanyId(@Param('companyId') companyId: string) {
+    const company = await this.loanService.findByCompany(companyId);
+
+    if (!company) {
+      throw new NotFoundException('Loans from this company were not found');
+    }
+  }
+
+  @Post()
+  async createLoan(@Body() createLoanDto: CreateLoanDto) {
+    const loan = await this.loanService.createLoan(createLoanDto);
+    return loan;
+  }
+}
--- src/loan/loan.service.ts
+++ src/loan/loan.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@nestjs/common';
-import { Employee, Loan } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { createLoanDto } from './dto/createLoanDto';
 import axios from 'axios';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+import { Employee, Loan } from '@prisma/client';
 import { generateDueDate } from 'src/utils/generateDueDate';
 import { minimumScoreChecker } from 'src/utils/minimumScoreChecker';
 import { EmployeeService } from 'src/employee/employee.service';
 import { CompanyService } from 'src/company/company.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateLoanDto } from './dto/CreateLoanDto';
 
 @Injectable()
 export class LoanService {
@@ -39,26 +44,29 @@ export class LoanService {
     return loans;
   }
 
-  async createLoan(createLoanDto: createLoanDto) {
-    const { amount, approved, parcelAmount, employeeId } = createLoanDto;
+  async createLoan(createLoanDto: CreateLoanDto) {
+    const { amount, parcelAmount, employeeId } = createLoanDto;
 
     if (parcelAmount < 1 || parcelAmount > 4) {
-      throw new Error('Parcel amount out of bounds');
+      throw new BadRequestException('Parcel amount is invalid');
     }
+
+    if (amount < 0)
+      throw new BadRequestException('Amount cannot be less than zero');
 
     const dueDate = generateDueDate(new Date());
 
     const employee = await this.employeeService.findById(employeeId);
 
-    if (!employee) throw new Error('Employee not found');
+    if (!employee) throw new NotFoundException('Employee not found');
 
     if (employee.companyId) {
       const company = await this.companyService.findById(employee.companyId);
 
-      if (!company)
-        throw new Error('User is not associated with a valid company');
-    } else if (!employee.companyId) {
-      throw new Error('User is not associated with a valid company');
+      if (!company || !employee.companyId)
+        throw new NotFoundException(
+          'User is not associated with a valid company',
+        );
     }
 
     const isScoreValid: boolean = await axios
@@ -69,10 +77,11 @@ export class LoanService {
         return minimumScoreChecker(response.data.score, employee.salary);
       })
       .catch(() => {
-        throw new Error('Failed to fetch score');
+        throw new ServiceUnavailableException('Failed to fetch score');
       });
 
-    if (!isScoreValid) throw new Error('Score is invalid for this operation');
+    if (!isScoreValid)
+      throw new BadRequestException('Score is invalid for this operation');
 
     const activeLoans = await this.prisma.loan.findMany({
       where: { employeeId },
@@ -88,7 +97,7 @@ export class LoanService {
     const loanLimit = employee?.salary * 0.35;
 
     if (totalApproved + amount > loanLimit) {
-      throw new Error('Loan requested surpasses loan limit');
+      throw new BadRequestException('Loan requested surpasses loan limit');
     }
 
     const isApproved: boolean = await axios
@@ -98,17 +107,19 @@ export class LoanService {
       .then((response) => {
         return response.data.status === 'aprovado' ? true : false;
       })
-      .catch((error) => {
-        console.log(error);
-        return false;
+      .catch(() => {
+        throw new ServiceUnavailableException('Failed to fetch service');
       });
 
-    if (!isApproved) throw new Error('Loan request not approved');
+    if (!isApproved)
+      throw new BadRequestException(
+        'Loan request not approved by the external service',
+      );
 
     const loan = await this.prisma.loan.create({
       data: {
         amount,
-        approved,
+        approved: true,
         dueDate,
         parcelAmount,
         employeeId,
